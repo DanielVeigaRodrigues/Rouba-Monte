a6dbd8fe37cef843f29c2fedb4b626a23b81b80b
Samuel Simonetti <642534@sga.pucminas.br>
2025-11-20 16:29:30 -0300

    feat: Baralho class with shuffle method

---
+++ Classes/Baralho.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Rouba_Monte.Classes
+{
+    public class Baralho
+    {
+        private List<Carta> _cartas;
+        public List<Carta> Cartas {get => _cartas; set { _cartas = value; } }
+
+        public Baralho()
+        {
+            _cartas = new List<Carta>();
+            int[] valores = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+            foreach (int valor in valores)
+            {
+                foreach(NaipeEnum naipe in Enum.GetValues(typeof(NaipeEnum)))
+                {
+                    _cartas.Add(new Carta(valor, naipe));
+                }
+            }
+
+            Cartas = _cartas;
+        }
+
+        public void Embaralhar()
+        {
+            List<Carta> newCartaList = new List<Carta>();
+            Carta[] cartasArray = Cartas.ToArray();
+            Random.Shared.Shuffle(cartasArray);
+
+            foreach(Carta carta in cartasArray)
+            {
+                newCartaList.Add(carta);
+            }
+
+            _cartas = newCartaList;
+        }
+    }
+}
