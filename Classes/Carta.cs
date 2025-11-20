add29a5293ffb758184a3bc8cf0ac8083d9af304
Samuel Simonetti <642534@sga.pucminas.br>
2025-11-20 16:29:01 -0300

    feat: Carta class with print and compare method

---
+++ Classes/Carta.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Rouba_Monte.Classes
+{
+    public enum NaipeEnum 
+    {
+        Copas,
+        Ouros,
+        Paus,
+        Espadas
+    }
+    public class Carta
+    {
+        private int _numero;
+
+        private NaipeEnum _naipe;
+        public int Numero {  get => _numero; set => _numero = value; }
+        public NaipeEnum Naipe { get => _naipe; set => _naipe = value; }
+
+
+        public Carta(int numero, NaipeEnum naipe)
+        {
+            _numero = numero;
+            _naipe = naipe;
+        }
+
+        public override string ToString()
+        {
+            string numTexto = _numero switch
+            {
+                1 => "A",
+                10 => "D",
+                11 => "Q",
+                12 => "J",
+                13 => "K",
+                _ => _numero.ToString(),
+            };
+
+            string naipeTexto = _naipe switch
+            {
+                NaipeEnum.Paus => "♣",
+                NaipeEnum.Espadas => "♠",
+                NaipeEnum.Ouros => "♦",
+                NaipeEnum.Copas => "♥",
+                _ => ""
+            };
+
+            return "_____"
+                + $"\n|{numTexto} {naipeTexto}|"
+                + $"\n| {naipeTexto} |"
+                + $"\n|{naipeTexto} {numTexto}|"
+                + "\n-----\n";
+        }
+
+        public bool CompararCartas(Carta carta)
+        {
+            // INFO: Igualdade simples no primeiro momento
+            return carta.Numero == _numero;
+        }
+
+    }
+}
