4d2a2f0305426178e0b0b78ca8e604055129c2b1
Samuel Simonetti <642534@sga.pucminas.br>
2025-11-20 16:30:13 -0300

    feat: Jogador class with utility method

---
+++ Classes/Jogador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Rouba_Monte.Classes
+{
+    public class Jogador
+    {
+        private string _nome;
+        private int _posicao;
+        private int _quantidadeCartas;
+        private Queue<int> _posicoes;
+
+        public string Nome { get => _nome; set => _nome = value; }
+        public int Posicao { get => _posicao; set => _posicao = value; }
+        public int QuantidadeCartas { get => _quantidadeCartas; set => _quantidadeCartas = value; }
+        public Queue<int> Posicoes { get => _posicoes; set => _posicoes = value; }
+
+        // INFO: atributo para gestão do jogo
+        public Stack<Carta> Monte { get; set; }
+
+        public Jogador(string nome)
+        {
+            _nome = nome;
+            _quantidadeCartas = 0;
+            _posicoes = new Queue<int>();
+            _posicao = -1;
+            Monte = new Stack<Carta>();
+        }
+
+        public Jogador(
+            string nome,
+            int posicao,
+            int quantidadeCartas,
+            int[] posicoes
+        )
+        { 
+            _nome = nome;
+            _posicao = posicao;
+            _quantidadeCartas = quantidadeCartas;
+            _posicoes = new Queue<int>();
+            foreach (int pos in posicoes)
+            {
+                _posicoes.Enqueue(pos);
+            }
+            Monte = new Stack<Carta>();
+        }
+
+        public Carta OlharTopoDoMonte()
+        {
+            return Monte.Peek();
+        }
+    }
+}
